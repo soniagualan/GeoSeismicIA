eefdbd0cb94dfbcd8be1e4d3aedd24ece20eed16
soniagualan <sonia6.gualan@hotmail.com>
2026-01-27 12:41:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -362,10 +362,10 @@ if archivo is not None:
                     pdf_path = "Reporte_GeoSismicIA.pdf"
 
                        # Guardar imagen original (preferir la que viene de n8n)
-                   if img_original_b64:
+                    if img_original_b64:
                       with open(temp_orig_path, "wb") as f:
                          f.write(base64.b64decode(img_original_b64))
-                   else:
+                    else:
                       with open(temp_orig_path, "wb") as f:
                          f.write(image_bytes)
                       with col_res1:
